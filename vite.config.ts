e386741c6281768a6fddcad5a1dda5ff65e1d93a
nahyun2.name <chlskgus789@gmail.com>
2026-05-31 18:30:53 +0900

    modified BE URL22

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     proxy: {
       // /api로 시작하는 요청을 실제 백엔드 서버로 우회
       "/api": {
-        target: "http://13.209.89.32:8080",
+        target: "http://15.164.100.139:8080",
         changeOrigin: true, // 호스트 헤더를 target으로 변경
       },
     },
